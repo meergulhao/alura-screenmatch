5f68d504fade58cb195e56a3d5522c4719fa99d2
Guilherme Mergulhão <golimpiof@gmail.com>
2024-10-15 12:45:15 -0300

    definindo construtores

--- src/Principal.java
+++ src/Principal.java
@@ -8,9 +8,7 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("Star Wars");
-        meuFilme.setAnoDeLancamento(1977);
+        Filme meuFilme = new Filme("Star Wars", 1977);
         meuFilme.setDuracaoEmMinutos(120);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -24,18 +22,14 @@ public class Principal {
 //        meuFilme.totalDeAvaliacoes = 1;
 //        System.out.println(meuFilme.pegaMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("Lost", 2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Minutos para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Interestelar");
-        outroFilme.setAnoDeLancamento(2014);
+        Filme outroFilme = new Filme("Interestelar", 2014);
         outroFilme.setDuracaoEmMinutos(160);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -53,10 +47,8 @@ public class Principal {
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
 
-        var maisUmFilme = new Filme();
+        var maisUmFilme = new Filme("O Tigre e a Neve", 2007);
         maisUmFilme.setDuracaoEmMinutos(120);
-        maisUmFilme.setNome("O Tigre e a Neve");
-        maisUmFilme.setAnoDeLancamento(2007);
         maisUmFilme.avalia(9);
 
         ArrayList<Filme> listaDeFilmes = new ArrayList<>();
